9a94490df39f02ed5147d75950174babb69e8f5e
Zalan12 <farkaszalan@turr.hu>
2025-09-09 12:20:08 +0200

    Regisztráció kialakítása, back és frontend összekötése,regexp

--- assets/controllers/userCtrl.js
+++ assets/controllers/userCtrl.js
@@ -1,13 +1,69 @@
-function registration()
-{
-    alert("OK");
-}
-function login(){}
+const passRegExp=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const emailRegExp=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-function logout(){}
 
-function getProfile(){}
+async function registration() {
+    /* await fetch('http://localhost:3000/users')
+     .then(res=> res.json())
+     .then(data=>console.log(data))*/
+    let nameField = document.querySelector("#nameField");
+    let emailField = document.querySelector("#emailField");
+    let passwordField = document.querySelector("#passwordField");
+    let confirmPasswordField=document.querySelector("#confirmPasswordField")
 
-function updateProfile(){}
+    
 
-function updatePassword(){}
+    if(nameField.value==''||emailField.value==''||passwordField.value==''||confirmPasswordField.value=='')
+    {alert("NEm adatal meg minden adatot")
+        return;
+    }
+    if(passwordField.value!=confirmPasswordField.value)
+    {
+        alert('A jelszavak nem egyeznek')
+        return;
+    }
+    if(!passRegExp.test(passwordField.value))
+        {
+            alert("A jelszó nem elég bizonságos! ")
+            return;
+        }
+    
+    if(!emailRegExp.test(emailField.value))
+        {
+            alert("Az email nem jós! ")
+            return;
+        }
+        
+
+    try{
+        const respond = await fetch('http://localhost:3000/users', {
+            method:"POST",
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body:
+            JSON.stringify({
+                name: nameField.value,
+                email: emailField.value,
+                password: passwordField.value,
+            }  
+        )
+        })
+        console.log('Státusz',respond.status)
+       
+        const data = await respond.json();
+        alert(data)
+    }  
+    catch(  err){
+        console.log("Hiba történt! ", err)
+    }}
+
+function login() { }
+
+function logout() { }
+
+function getProfile() { }
+
+function updateProfile() { }
+
+function updatePassword() { }
