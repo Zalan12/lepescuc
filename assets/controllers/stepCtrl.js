3a95e0cd0a8ffa117ba28e04b81f82569577221b
Zalan12 <farkaszalan@turr.hu>
2025-09-16 10:32:18 +0200

    Haza mentem beteg lettem

--- assets/controllers/stepCtrl.js
+++ assets/controllers/stepCtrl.js
@@ -1,3 +1,5 @@
+let adat=[];
+let editMode=true
 
 function setDate()
 {
@@ -46,6 +48,55 @@ async function add()
     catch(err){
         console.log('Hiba történt: ', err)
     }
+    let idx=adat.findIndex(step=>step.date==date && step.uid==loggedUser.id);
+    if(idx==-1)
+        {
+            try{
+                    let res =await fetch(`${ServerURL}/steps`,{
+                        method: 'POST',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body: JSON.stringify({
+                            uid:loggedUser.id,
+                            datum:date,
+                            lepes:lepes
+                        })
+                    })
+
+                    let data=await res.json();
+                    if(res.status==200){
+                        showMessage('success','Siker',data.msg)
+                        await loadData();
+                    }
+
+            }
+            catch(err){console.log(err);}
+
+        }
+    else{
+        try{
+            let res =await fetch(`${ServerURL}/steps/${adat[idx].id}`,{
+                method: 'PATCH',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({
+                    uid:loggedUser.id,
+                    datum:date,
+                    lepes:Number(adat[idx].lepes)+Number(lepes)
+                })
+            })
+
+            let data=await res.json();
+            if(res.status==200){
+                showMessage('success','Siker',data.msg)
+                await loadData();
+            }
+
+    }
+    catch(err){console.log(err);}
+    }
 
 }
 async function loadData() {
@@ -53,37 +104,43 @@ async function loadData() {
     try{
         const res=await fetch(`${ServerURL}/steps/user/${loggedUser.id}`);
         if(!res.ok){showMessage('danger','Hiba','Baj van gec');return};
-        const adat=await res.json();
+        adat=await res.json();
+        
         let adatTabla=document.querySelector('#adatTabla');
         let osszLepes=document.querySelector('#osszLepes');
         let osszLepesSzam=0;
         for(let i=0;i<adat.length;i++)
             {
+                
                 let tr=document.createElement('tr');
                 let td1=document.createElement('td');       
                 let td2=document.createElement('td');
                 let td3=document.createElement('td');
                 let td4=document.createElement('td');
-                let btn=document.createElement('button');
-
+                let delBtn=document.createElement('button');
+                let editBtn=document.createElement('button');
+                editBtn.classList.add('btn','btn-warning','btn-sm','me-2')
 
                 td1.classList.add('text-end');
                 td2.classList.add('text-end');
                 td3.classList.add('text-end');
                 td4.classList.add('text-end');
-                btn.classList.add('btn','btn-danger');
-                btn.addEventListener('click', () => {
+                delBtn.classList.add('btn','btn-danger');
+                delBtn.addEventListener('click', () => {
                     egyTorles();
                 })
 
+                editBtn.addEventListener('click',()=>{deleteStep(index)})
                 td1.innerHTML=adat[i].sid;
                 td2.innerHTML=adat[i].datum;
                 td3.innerHTML=adat[i].lepes;
-                btn.textContent='X';
+                delBtn.innerHTML='<i class="bi bi-trash-fill"></i>';
+                editBtn.innerHTML='<i class="bi bi-pencil-fill"></i>';
                 tr.appendChild(td1);
                 tr.appendChild(td2);
                 tr.appendChild(td3);
-                td4.appendChild(btn)
+                td4.appendChild(delBtn);
+                td4.appendChild(editBtn);
                 tr.appendChild(td4);
                 adatTabla.appendChild(tr);
                 osszLepesSzam+=adat[i].lepes;
@@ -100,7 +157,44 @@ async function loadData() {
 
     
 
+
+}
+
+async function deleteStep(index)
+{
+    /*if(confirm('Biztosan törlöd az adatot?')){
+
+        try{let res=await fetch(`${ServerURL}/steps/${adat[index].uid}`,{
+            method:'DELETE',
+            headers:{'Content-Type':'application/json'}
+        })}
+    }*/
 }
 async function egyTorles() {
     showMessage('info','Fejlesztés alatt','Ez a funkció még fejlesztés alatt áll')
+}
+
+function toggleMode(editMode)
+{
+    let addBtn=document.querySelector('#addBtn')
+    let editBtn=document.querySelector('#editBtn')
+    let deleteBtn=document.querySelector('#deleteBtn')
+    let cancelBtn=document.querySelector('#cancelBtn')
+    if(mode)
+        {
+            addBtn.classList.add('hide');
+            deleteBtn.classList.remove('hide')
+            cancelBtn.classList.remove('hide')
+            updateBtn.classList.remove('hide')
+        }
+
+        else
+            {
+                addBtn.classList.remove('hide');
+                deleteBtn.classList.add('hide')
+                cancelBtn.classList.add('hide')
+                updateBtn.classList.add('hide')
+            }
+
+        
 }
